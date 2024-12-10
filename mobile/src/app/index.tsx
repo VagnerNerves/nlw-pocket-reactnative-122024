6ed8e3ada22ecd64d03be94617be9b991b533890
Vagner Nerves <vagnernervessantos@gmail.com>
2024-12-09 21:24:23 -0300

    feat: Add Welcome on index.

--- mobile/src/app/index.tsx
+++ mobile/src/app/index.tsx
@@ -1,9 +1,10 @@
+import { Welcome } from "@/components/welcome";
 import { Text, View } from "react-native";
 
-export default function Home() {
+export default function Index() {
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Project Nearby</Text>
+    <View style={{ flex: 1, padding: 40, gap: 40 }}>
+      <Welcome />
     </View>
   );
 }
